50ebc489e5c700bc86154a13a242489713d9152e
zzzmini <sm01079200887@gmail.com>
2025-09-03 11:06:47 +0900

    시큐리티 필터 체인 설정

---
+++ src/main/java/com/my/securityTest/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.my.securityTest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainPage() {
+        return "main";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin";
+    }
+}
