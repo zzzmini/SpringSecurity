dde60ff7a360aa21cda2cce09c80d77a027b51a5
zzzmini <sm01079200887@gmail.com>
2025-09-03 12:52:02 +0900

    시큐리티 로그인 완성

--- src/main/java/com/my/securityTest/repository/UserRepository.java
+++ src/main/java/com/my/securityTest/repository/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     // 아이디가 존재하는지 확인하는 쿼리메서드
     boolean existsByUsername(String username);
+
+    // username으로 검색 한 결과 리턴
+    UserEntity findByUsername(String username);
 }
