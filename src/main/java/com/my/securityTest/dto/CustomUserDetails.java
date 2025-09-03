d10bd9d549b84ab0c46f6b97b6a68fcd79c71bd4
zzzmini <sm01079200887@gmail.com>
2025-09-03 14:38:25 +0900

    타임리프처리

--- src/main/java/com/my/securityTest/dto/CustomUserDetails.java
+++ src/main/java/com/my/securityTest/dto/CustomUserDetails.java
@@ -2,6 +2,7 @@ package com.my.securityTest.dto;
 
 import com.my.securityTest.entity.UserEntity;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
@@ -17,15 +18,7 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> collection = new ArrayList<>();
-
-        collection.add(new GrantedAuthority() {
-            @Override
-            public String getAuthority() {
-                return String.valueOf(userEntity.getRole());
-            }
-        });
-        return collection;
+        return List.of(new SimpleGrantedAuthority(userEntity.getRole().name()));
     }
 
     @Override
